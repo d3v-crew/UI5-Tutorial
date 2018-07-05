ad11c3e9f4468f10d9325745e8ecc4f1e939f7c3
Dominic Evers <evers.docdata@gmail.com>
2018-07-05 09:19:21 +0000

    Revert "add internationalisation"
    This reverts commit 381277cb1857c7e0c7509b76f24e238e2af527aa.

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,9 +1,8 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-    ], function(Controller, MessageToast, JSONModel, ResourceModel) {
+    "sap/ui/model/json/JSONModel"
+    ], function(Controller, MessageToast, JSONModel) {
         "use strict";
         return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
             onInit:function(){
@@ -12,16 +11,9 @@ sap.ui.define([
               };
               var oModel = new JSONModel(oData);
               this.getView().setModel(oModel);
-              var i18nModel = new ResourceModel({
-                  bundleName : "sap.ui.demo.walkthrough.i18n.i18n"
-              });
-              this.getView().setModel(i18nModel, "i18n");
             },
             onShowHello : function(){
-                var oBundle = this.getView().getModel("i18n").getResourceBundle();
-                var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-                var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-                MessageToast.show(sMsg);
+                MessageToast.show("Hello World");
             }
         });
     });
